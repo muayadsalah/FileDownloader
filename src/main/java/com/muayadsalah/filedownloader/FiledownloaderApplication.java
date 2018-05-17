c33f8e99a23089b8329970c7d52ba83e998fe7ca
muayadsalah <muayadsala7@gmail.com>
2018-05-17 17:52:58 +0300

    Initial project structure

---
+++ src/main/java/com/muayadsalah/filedownloader/FiledownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.muayadsalah.filedownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledownloaderApplication.class, args);
+	}
+}
